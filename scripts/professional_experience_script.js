918ef53606c1b052dc5f0cfbe62aee40e78909f9
= <chantel@mybond.com.au>
2025-04-06 11:52:05 +1000

    add work experience in + js and css for info about what i did

---
+++ scripts/professional_experience_script.js
@@ -0,0 +1,12 @@
+// // https://www.youtube.com/watch?v=HKapGix6sis
+
+// var thales_info = document.getElementById("thales_title");
+
+// //  add a listener
+// thales_info.addEventListener('onclick', () => {
+//     console.log('I was clicked :d');
+// });
+
+// var thales_info = document.getElementById('thales_title');
+
+// console.log(thales_info);
